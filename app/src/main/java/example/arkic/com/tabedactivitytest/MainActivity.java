992bd7e4dc8166b816d010cdf91c1ca799526603
623251942@qq.com <a313548323>
2015-12-07 19:56:29 +0800

    First

--- app/src/main/java/example/arkic/com/tabedactivitytest/MainActivity.java
+++ app/src/main/java/example/arkic/com/tabedactivitytest/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
 
         // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
+        // primary sections of the activity.qweq
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
